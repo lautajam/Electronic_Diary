e31b6adbb68e4938ed3ebdd2331cde5c60429bf1
lautajam <lautajamdev@gmail.com>
2023-09-20 23:17:17 -0300

    Upgrade frontend
    I arranged the txt files a bit and made the screen smaller

--- src/GUI/Display_EDiary.java
+++ src/GUI/Display_EDiary.java
@@ -1,9 +1,11 @@
-package GUI;
 
+package GUI;
 
 public class Display_EDiary extends javax.swing.JFrame {
 
-
+    /**
+     * Creates new form DIsplay_Data
+     */
     public Display_EDiary() {
         initComponents();
     }
@@ -21,11 +23,6 @@ public class Display_EDiary extends javax.swing.JFrame {
         lbl_telefono = new javax.swing.JLabel();
         lbl_fnac = new javax.swing.JLabel();
         txt_dni = new javax.swing.JTextField();
-        txt_surname = new javax.swing.JTextField();
-        txt_name = new javax.swing.JTextField();
-        txt_address = new javax.swing.JTextField();
-        txt_telephone = new javax.swing.JTextField();
-        txt_birthday = new javax.swing.JTextField();
         sep_title_txt = new javax.swing.JSeparator();
         sep_txt_save = new javax.swing.JSeparator();
         btn_previous = new javax.swing.JButton();
@@ -33,6 +30,11 @@ public class Display_EDiary extends javax.swing.JFrame {
         btn_save = new javax.swing.JButton();
         lbl_index = new javax.swing.JLabel();
         txt_index = new javax.swing.JTextField();
+        txt_dni1 = new javax.swing.JTextField();
+        txt_dni2 = new javax.swing.JTextField();
+        txt_dni3 = new javax.swing.JTextField();
+        txt_dni4 = new javax.swing.JTextField();
+        txt_dni5 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,72 +68,67 @@ public class Display_EDiary extends javax.swing.JFrame {
         btn_save.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         btn_save.setText("SAVE");
 
-        lbl_index.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        lbl_index.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         lbl_index.setText("Index:");
 
-        txt_index.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        txt_index.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
+                                .addGap(233, 233, 233)
+                                .addComponent(lbl_title)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(sep_title_txt))
+                                        .addComponent(sep_txt_save, javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
                                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                                .addComponent(lbl_nombre)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(txt_dni2, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                                .addComponent(lbl_DNI)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(txt_dni3, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))
                                                         .addGroup(jPanel1Layout.createSequentialGroup()
                                                                 .addComponent(lbl_apellido)
-                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                                .addComponent(txt_surname, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                                        .addComponent(lbl_nombre)
-                                                                        .addComponent(lbl_DNI))
-                                                                .addGap(40, 40, 40)
-                                                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                                        .addComponent(txt_dni)
-                                                                        .addComponent(txt_name))))
+                                                                .addGap(18, 18, 18)
+                                                                .addComponent(txt_dni1, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                                 .addGap(18, 18, 18)
                                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(lbl_direccion)
                                                         .addComponent(lbl_telefono)
-                                                        .addComponent(lbl_fnac))
-                                                .addGap(18, 18, Short.MAX_VALUE)
-                                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                                        .addComponent(txt_telephone)
-                                                        .addComponent(txt_address)
-                                                        .addComponent(txt_birthday, javax.swing.GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE))
-                                                .addGap(22, 22, 22))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGap(227, 227, 227)
-                                                .addComponent(lbl_title)
-                                                .addGap(0, 0, Short.MAX_VALUE))))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(15, 15, 15)
+                                                        .addComponent(lbl_fnac)
+                                                        .addComponent(lbl_direccion))
+                                                .addGap(18, 18, 18)
+                                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(txt_dni5, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(txt_dni4, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(txt_dni, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                .addContainerGap())
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addComponent(sep_txt_save)
-                                                .addGap(22, 22, 22))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addComponent(sep_title_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 733, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(0, 22, Short.MAX_VALUE))))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(284, 284, 284)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGap(41, 41, 41)
-                                                .addComponent(lbl_index)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                .addComponent(txt_index, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                                 .addComponent(btn_previous)
-                                                .addGap(18, 18, 18)
-                                                .addComponent(btn_save)
-                                                .addGap(18, 18, 18)
-                                                .addComponent(btn_next)))
-                                .addGap(0, 0, Short.MAX_VALUE))
+                                                .addGap(29, 29, 29))
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addComponent(lbl_index)
+                                                .addGap(9, 9, 9)))
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(txt_index)
+                                        .addComponent(btn_save, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(29, 29, 29)
+                                .addComponent(btn_next)
+                                .addGap(215, 215, 215))
         );
         jPanel1Layout.setVerticalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -143,21 +140,21 @@ public class Display_EDiary extends javax.swing.JFrame {
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(lbl_DNI)
+                                        .addComponent(txt_dni3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(lbl_direccion)
-                                        .addComponent(txt_dni, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(txt_address, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(64, 64, 64)
+                                        .addComponent(txt_dni5, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(lbl_telefono)
                                         .addComponent(lbl_nombre)
-                                        .addComponent(txt_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(txt_telephone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(61, 61, 61)
+                                        .addComponent(txt_dni, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(txt_dni2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(lbl_fnac)
                                         .addComponent(lbl_apellido)
-                                        .addComponent(txt_surname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(txt_birthday, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(txt_dni1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lbl_fnac)
+                                        .addComponent(txt_dni4, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
                                 .addComponent(sep_txt_save, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
@@ -169,7 +166,7 @@ public class Display_EDiary extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(lbl_index)
                                         .addComponent(txt_index, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(24, Short.MAX_VALUE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -207,12 +204,12 @@ public class Display_EDiary extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_title;
     private javax.swing.JSeparator sep_title_txt;
     private javax.swing.JSeparator sep_txt_save;
-    private javax.swing.JTextField txt_address;
-    private javax.swing.JTextField txt_birthday;
     private javax.swing.JTextField txt_dni;
+    private javax.swing.JTextField txt_dni1;
+    private javax.swing.JTextField txt_dni2;
+    private javax.swing.JTextField txt_dni3;
+    private javax.swing.JTextField txt_dni4;
+    private javax.swing.JTextField txt_dni5;
     private javax.swing.JTextField txt_index;
-    private javax.swing.JTextField txt_name;
-    private javax.swing.JTextField txt_surname;
-    private javax.swing.JTextField txt_telephone;
     // End of variables declaration
 }
